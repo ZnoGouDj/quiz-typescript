44205d606d64fae221db8bedce10115bbb5fe351
ZnoGouDj <znogoud3@gmail.com>
2022-03-23 20:40:31 +0100

    Fix the API fetch

--- src/API.ts
+++ src/API.ts
@@ -1,11 +1,15 @@
+import { shuffleArray } from './utils';
+
 export type Question = {
   category: string;
-  correct_answer: string;
+  correct_answers: string;
   difficulty: string;
-  incorrect_answer: string[];
+  incorrect_answers: string[];
   question: string;
   type: string;
 };
+
+export type QuestionState = Question & { answers: string[] };
 
 export enum Difficulty {
   EASY = 'easy',
@@ -14,7 +18,17 @@ export enum Difficulty {
 }
 
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
-  const endpoint = `https://opentdb.com/api/php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
-  const data = await (await fetch(endpoint)).json();
+  const endpoint = `https://quizapi.io/api/v1/questions?limit=${amount}&difficulty=${difficulty}`;
+  const response = await fetch(endpoint, {
+    method: 'GET',
+    headers: {
+      'X-API-KEY': '8W92xbm2eZTPY3ImHFj8cbsdpNEnXW5O0GCg7VoP',
+    },
+  });
+  const data = await response.json();
   console.log(data);
+  return data.results.map((question: Question) => ({
+    ...question,
+    answer: shuffleArray([...question.incorrect_answers, question.correct_answers]),
+  }));
 };
